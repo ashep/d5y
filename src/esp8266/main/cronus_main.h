d2058dacfa4a6995c7c447dcbb09a5f48de20fb6
Alexander Shepetko <a@shepetko.com>
2020-08-27 23:30:05 +0300

    WIP on ESP8266 part

---
+++ src/esp8266/main/cronus_main.h
@@ -0,0 +1,12 @@
+#ifndef _CRONUS_MAIN_H_
+#define _CRONUS_MAIN_H_
+
+#define WIFI_AP_SSID "cronus"
+#define WIFI_AP_PASS "cronus20"
+#define WIFI_AP_MAX_STA_CONN 1
+
+void init_wifi();
+void init();
+void app_main();
+
+#endif
