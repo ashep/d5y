1bd549bcb83dcf02fc55af4779f7b5cf92d2395a
Oleksandr Shepetko <a@shepetko.com>
2024-05-23 22:49:45 +0300

    Cleanup

--- components/dy_widget/include/dy/widget.h
+++
@@ -1,8 +0,0 @@
-#ifndef DY_WIDGET_H
-#define DY_WIDGET_H
-
-#include "dy/error.h"
-
-dy_err_t dy_widget_init();
-
-#endif // DY_WIDGET_H
--- components/dy_widget/include/dy/widget/clock.h
+++
@@ -1,8 +0,0 @@
-#ifndef DY_WIDGET_CLOCK_H
-#define DY_WIDGET_CLOCK_H
-
-#include "dy/error.h"
-
-dy_err_t dy_widget_clock_init();
-
-#endif // DY_WIDGET_CLOCK_H
