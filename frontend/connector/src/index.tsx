95b7726cf9f38507f01cc4a1aa4e70dde936dc3b
Oleksandr Shepetko <a@shepetko.com>
2024-05-23 21:55:55 +0300

    Move out frontend

--- frontend/connector/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import '@fontsource/roboto';
-import {render} from 'preact';
-// import './style.css';
-import './i18n';
-import App from './components/App'
-
-render(<App/>, document.getElementById('app'));
